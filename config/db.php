c626fd995992f4ab28d0ffe862d4e2bb8b02f5d9
Subs <subarnat62@gmail.com>
2026-02-01 17:24:46 +0545

    added logout and search

--- config/db.php
+++ config/db.php
@@ -1,9 +1,31 @@
+
+
+
 <?php
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=recipe_db;charset=utf8mb4",
-    "root",
-    "",
-    [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]
-);
+$isLocal = ($_SERVER['HTTP_HOST'] === 'localhost');
+if ($isLocal) {
+    $host     = "localhost";
+    $dbname   = "recipe_db";
+    $username = "root";
+    $password = "";
+}
+else {
+    $host     = "localhost";
+    $dbname   = "np03cs4s250047";
+    $username = "np03cs4s250047";
+    $password = "xN31VbEggj";
+}
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ]
+    );
+} catch (PDOException $e) {
+    die("Database connection unsuccessful.");
+}
