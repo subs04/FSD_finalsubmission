9f126e7a81924aa62278a028ef63a5dc3c821d6e
Subs <subarnat62@gmail.com>
2026-02-02 11:14:04 +0545

    final commit

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,6 @@ require_once __DIR__ . "/functions.php";
     <a href="search.php">Search</a> |
     <a href="logout.php">Logout</a>
 <?php else: ?>
-    <a href="login.php">Admin Login</a>
 <?php endif; ?>
 </nav>
 <hr>
